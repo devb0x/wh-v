ad5e563d76ad931d2bae59a8a1eb99aca1562a53
devb0x <78238534+devb0x@users.noreply.github.com>
2024-11-30 10:38:22 +0100

    trying to fix apiUrl issue

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -5,6 +5,7 @@ import { HowItWorksComponent } from "../../components/features/how-it-works/how-
 import { CarouselComponent } from "../../components/features/carousel/carousel.component"
 import { DividerComponent } from "../../layout/divider/divider.component"
 import { RecentUploadsComponent } from "../../components/features/recent-uploads/recent-uploads.component"
+import {environment} from "../../../environments/environment";
 
 @Component({
 	selector: 'app-homepage',
@@ -19,5 +20,8 @@ import { RecentUploadsComponent } from "../../components/features/recent-uploads
 	styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
+	ngOnInit() {
+		console.log('API URL:', environment.apiUrl);
+	}
 
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+	production: true,
+	apiUrl: 'https://wharmy.vercel.app/api'
+};
