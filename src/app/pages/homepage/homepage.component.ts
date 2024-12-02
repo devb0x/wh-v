be5b6f25e485d47f2532f246f4d93aa222eeaf0b
devb0x <78238534+devb0x@users.noreply.github.com>
2024-12-02 09:03:08 +0100

    checking apiUrl vercel

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core'
 
 import { HeroSectionComponent } from "../../components/features/hero-section/hero-section.component"
 import { HowItWorksComponent } from "../../components/features/how-it-works/how-it-works.component"
-import { CarouselComponent } from "../../components/features/carousel/carousel.component"
 import { DividerComponent } from "../../layout/divider/divider.component"
 import { RecentUploadsComponent } from "../../components/features/recent-uploads/recent-uploads.component"
 import {environment} from "../../../environments/environment";
@@ -21,6 +20,7 @@ import {environment} from "../../../environments/environment";
 })
 export class HomepageComponent {
 	ngOnInit() {
+		console.log('log from homepage, checking apiUrl')
 		console.log('API URL:', environment.apiUrl);
 	}
 
